eb60083e0494f262be635d7551e620a196857ca1
Mark John Buenconsejo <mjwork@simpleteq.com>
2010-10-07 14:00:30 +0800

    added backward compatibility with use of helpers

--- lib/palmade/couch_potato/helpers.rb
+++ lib/palmade/couch_potato/helpers.rb
@@ -1,5 +1,14 @@
 module Palmade::CouchPotato
   module Helpers
+    def self.use(helper_name, *args)
+      mod = const_get(helper_name)
+      if args.size > 0
+        mod.setup(*args)
+      else
+        mod
+      end
+    end
+
     autoload :RailsHelper, File.join(COUCH_POTATO_LIB_DIR, 'couch_potato/helpers/rails_helper')
     autoload :Rails2Helper, File.join(COUCH_POTATO_LIB_DIR, 'couch_potato/helpers/rails2_helper')
   end
